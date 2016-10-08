07e88942b47ff82a2278f58c6b4dbf82b073d9a0
Paweł Fertyk <pfertyk@openmailbox.org>
2016-10-08 23:40:20 +0200

    Add test for adding text to list

--- test_main.py
+++ test_main.py
@@ -3,3 +3,9 @@ from main import TextHolder
 
 def test_text_list_is_empty_by_defaut():
     assert TextHolder().texts == []
+
+
+def test_can_add_text_to_list():
+    text_holder = TextHolder()
+    text_holder.add_text('hello')
+    assert text_holder.texts == ['hello']
