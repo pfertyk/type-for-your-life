38fb977b9ceb7b38bd7e1b2d9cf0609ac513aa86
Paweł Fertyk <pfertyk@openmailbox.org>
2016-10-08 23:52:01 +0200

    Add test for adding the text again

--- test_main.py
+++ test_main.py
@@ -1,4 +1,5 @@
 from main import TextHolder
+import pytest
 
 
 def test_text_list_is_empty_by_defaut():
@@ -9,3 +10,10 @@ def test_can_add_text_to_list():
     text_holder = TextHolder()
     text_holder.add_text('hello')
     assert text_holder.texts == ['hello']
+
+
+def test_cannot_add_the_same_text_twice():
+    text_holder = TextHolder()
+    text_holder.add_text('hello')
+    with pytest.raises(Exception):
+        text_holder.add_text('hello')
