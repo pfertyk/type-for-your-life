4f6b2e7dad7522aeafe44de7697467fd41e35a6b
Paweł Fertyk <pfertyk@openmailbox.org>
2016-10-09 18:32:16 +0200

    Add test for not calling rcc when phrase matches

--- test_callbacks.py
+++ test_callbacks.py
@@ -20,6 +20,14 @@ def test_rcc_called_when_no_phrase_matches_first_character(mocker):
     rcc.assert_called_with('a')
 
 
+def test_rcc_not_called_when_phrase_matches_first_character(mocker):
+    rcc = mocker.stub()
+    phrase_holder = PhrasesHolder(rejected_char_callback=rcc)
+    phrase_holder.add_phrase('hello')
+    phrase_holder.send_char('h')
+    rcc.assert_not_called()
+
+
 def test_rcc_called_when_subsequent_character_is_incorrect(mocker):
     rcc = mocker.stub()
     phrase_holder = PhrasesHolder(rejected_char_callback=rcc)
