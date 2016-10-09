a3af150f49eff6c598bf4658b58f3df69d97e5cc
Paweł Fertyk <pfertyk@openmailbox.org>
2016-10-09 23:33:08 +0200

    Display simple text

--- game.py
+++ game.py
@@ -4,10 +4,23 @@ pygame.init()
 pygame.mixer.quit()
 screen = pygame.display.set_mode((400, 300))
 done = False
+font_color = (0, 0, 0)
+background_color = (255, 255, 255)
+
+phrase = 'Hello world!'
+
+background = pygame.Surface(screen.get_size())
+background = background.convert()
+background.fill(background_color)
+
+font = pygame.font.Font(None, 36)
+text = font.render(phrase, 1, font_color)
+background.blit(text, text.get_rect())
 
 while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
 
+    screen.blit(background, (0, 0))
     pygame.display.flip()
