78b85f64ac89d443fed1e3a922cb6b8bef488cd4
Paweł Fertyk <pfertyk@openmailbox.org>
2016-10-09 18:33:57 +0200

    Add test for not calling acc if char doesn't match

--- test_callbacks.py
+++ test_callbacks.py
@@ -49,6 +49,14 @@ def test_can_define_accepted_char_callback():
     PhrasesHolder(accepted_char_callback=lambda a, b, c: None)
 
 
+def test_acc_not_called_when_first_character_does_not_match(mocker):
+    acc = mocker.stub()
+    phrase_holder = PhrasesHolder(accepted_char_callback=acc)
+    phrase_holder.add_phrase('hello')
+    phrase_holder.send_char('x')
+    acc.assert_not_called()
+
+
 def test_acc_called_when_first_character_matches(mocker):
     acc = mocker.stub()
     phrase_holder = PhrasesHolder(accepted_char_callback=acc)
