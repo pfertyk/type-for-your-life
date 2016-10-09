6927c3b4caddcb20ad88a66ea10ca76ba8e7c333
Paweł Fertyk <pfertyk@openmailbox.org>
2016-10-09 17:43:24 +0200

    Add test for rcc called when nothing matches

--- test_callbacks.py
+++ test_callbacks.py
@@ -6,7 +6,15 @@ def test_can_define_rejected_char_callback():
 
 
 def test_rcc_called_when_no_phrase_is_available(mocker):
-    rcc = mocker.stub(name='hello')
+    rcc = mocker.stub()
     phrase_holder = PhrasesHolder(rejected_char_callback=rcc)
+    phrase_holder.send_char('a')
+    rcc.assert_called_with('a')
+
+
+def test_rcc_called_when_no_phrase_matches_first_character(mocker):
+    rcc = mocker.stub()
+    phrase_holder = PhrasesHolder(rejected_char_callback=rcc)
+    phrase_holder.add_phrase('hello')
     phrase_holder.send_char('a')
     rcc.assert_called_with('a')
