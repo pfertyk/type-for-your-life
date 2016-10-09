35d3d69a0d3c3cd14c3c793c38dd99b840a8cecd
Paweł Fertyk <pfertyk@openmailbox.org>
2016-10-09 17:38:19 +0200

    Add test for calling rcc with empty phrases

--- test_callbacks.py
+++ test_callbacks.py
@@ -3,3 +3,10 @@ from main import PhrasesHolder
 
 def test_can_define_rejected_char_callback():
     PhrasesHolder(rejected_char_callback=lambda x: None)
+
+
+def test_rcc_called_when_no_phrase_is_available(mocker):
+    rcc = mocker.stub(name='hello')
+    phrase_holder = PhrasesHolder(rejected_char_callback=rcc)
+    phrase_holder.send_char('a')
+    rcc.assert_called_with('a')
