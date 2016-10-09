1cff3a20b80056f3e419ff25ad67b77c1d29231c
Paweł Fertyk <pfertyk@openmailbox.org>
2016-10-09 17:55:14 +0200

    Add test for calling rcc if char is incorrect

--- test_callbacks.py
+++ test_callbacks.py
@@ -18,3 +18,13 @@ def test_rcc_called_when_no_phrase_matches_first_character(mocker):
     phrase_holder.add_phrase('hello')
     phrase_holder.send_char('a')
     rcc.assert_called_with('a')
+
+
+def test_rcc_called_when_subsequent_character_is_incorrect(mocker):
+    rcc = mocker.stub()
+    phrase_holder = PhrasesHolder(rejected_char_callback=rcc)
+    phrase_holder.add_phrase('hello')
+    phrase_holder.send_char('h')
+    rcc.assert_not_called()
+    phrase_holder.send_char('h')
+    rcc.assert_called_with('h')
