4e76d4731d47303a5aa86f3a6b70af0108290892
Paweł Fertyk <pfertyk@openmailbox.org>
2016-10-09 23:49:39 +0200

    End game after typing whole phrase

--- game.py
+++ game.py
@@ -1,4 +1,6 @@
 import pygame
+from main import PhrasesHolder
+
 
 pygame.init()
 pygame.mixer.quit()
@@ -8,6 +10,21 @@ font_color = (0, 0, 0)
 background_color = (255, 255, 255)
 
 phrase = 'Hello world!'
+
+
+def accept_char(char, phrase, phrase_left):
+    print('Accepted', char)
+    if not phrase_left:
+        global done
+        done = True
+
+
+def reject_char(char):
+    print('Rejected', char)
+
+
+phrase_holder = PhrasesHolder(reject_char, accept_char)
+phrase_holder.add_phrase(phrase)
 
 background = pygame.Surface(screen.get_size())
 background = background.convert()
@@ -21,6 +38,9 @@ while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
+        elif event.type == pygame.KEYDOWN:
+            if event.unicode:
+                phrase_holder.send_char(event.unicode)
 
     screen.blit(background, (0, 0))
     pygame.display.flip()
