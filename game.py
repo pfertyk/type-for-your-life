9b706ea893a64b373c2c123a1c3b48305c1a9fe3
Paweł Fertyk <pfertyk@openmailbox.org>
2016-10-29 22:57:37 +0200

    Remove slots, place new phrase randomly

--- game.py
+++ game.py
@@ -8,15 +8,18 @@ WIDTH = 1280
 HEIGHT = 720
 FPS = 30
 
-SLOT_COUNT = 2
 MAX_PHRASES = 4
+FONT_SIZE = 36
+
+NEW_PHRASE_MAX_AVAILABLE_HEIGHT = 80
+NEW_PHRASE_SAFE_SPACE = 64
 
 
 class Phrase:
-    def __init__(self, phrase, topleft):
+    def __init__(self, phrase, midleft):
         self.padding = 10
         self.frame_width = 4  # TODO: extract to settings?
-        self.font = pygame.font.Font(None, 36)
+        self.font = pygame.font.Font(None, FONT_SIZE)
 
         self.full_phrase = self.font.render(
             phrase,
@@ -34,9 +37,10 @@ class Phrase:
             self.full_phrase.get_height() + 2 * self.padding
         )
 
-        self.frame = pygame.Rect(topleft, phrase_size)
+        self.frame = pygame.Rect((0, 0), phrase_size)
+        self.frame.midleft = midleft
 
-        self.topleft = topleft
+        self.topleft = self.frame.topleft
         self.text = phrase
 
     def update(self, phrase_left):
@@ -72,7 +76,7 @@ class Phrase:
 
 class PygamePhraseHolder:
     def __init__(self):
-        self.font = pygame.font.Font(None, 36)
+        self.font = pygame.font.Font(None, FONT_SIZE)
 
         self.available_phrases = [
             'How big?',
@@ -85,17 +89,10 @@ class PygamePhraseHolder:
         self.phrase_count = 0
         self.used_phrases = set()
         self.last_phrase_time = None
-        self.phrase_interval = 1000
+        self.phrase_interval = 4000
 
         self.stream = []
         self.phrase_to_stream = {}
-
-        self.slots = []
-
-        for i in range(SLOT_COUNT):
-            self.slots.append(
-                pygame.Rect(WIDTH - 10, 100 + HEIGHT//4*i, 20, 20)
-            )
 
         self.phrase_holder = PhrasesHolder(self.reject_char, self.accept_char)
 
@@ -110,18 +107,26 @@ class PygamePhraseHolder:
             if current_time - self.last_phrase_time < self.phrase_interval:
                 return
 
-        available_slots = []
-        for slot in self.slots:
+        available_midleft = None
+
+        for _ in range(10):
+            available_midleft = (WIDTH, random.randint(
+                NEW_PHRASE_SAFE_SPACE//2,
+                NEW_PHRASE_MAX_AVAILABLE_HEIGHT - NEW_PHRASE_SAFE_SPACE//2
+            ))
+
+            rect = pygame.Rect(
+                0, 0, NEW_PHRASE_SAFE_SPACE, NEW_PHRASE_SAFE_SPACE
+            )
+            rect.center = available_midleft
+
             for item in self.stream:
-                if slot.colliderect(item.get_rect()):
+                if rect.colliderect(item.get_rect()):
                     break
             else:
-                available_slots.append(slot)
-
-        if not available_slots:
+                break
+        else:
             return
-
-        slot = random.choice(available_slots)
 
         while True:
             phrase = random.choice(self.available_phrases)
@@ -133,7 +138,7 @@ class PygamePhraseHolder:
                 self.phrase_holder.add_phrase(phrase)
                 self.used_phrases.add(phrase)
 
-                item = Phrase(phrase, slot.topleft)
+                item = Phrase(phrase, available_midleft)
 
                 self.stream.append(item)
                 self.phrase_to_stream[phrase] = item
@@ -169,9 +174,6 @@ class PygamePhraseHolder:
                 print('Sorry, you lost!')
                 self.done = True
                 break
-
-        for slot in self.slots:
-            pygame.draw.rect(background, colors.SLOT, slot)
 
 
 pygame.init()
